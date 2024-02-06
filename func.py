a1633d35c39052a6b03b51dc175bdaa78f9266fc
J Mertig <jesusmertig@gmail.com>
2024-02-06 16:52:17 -0600

    Creates csv file containing all players on fantasy team

---
+++ func.py
@@ -0,0 +1,2 @@
+def get_bbref_key(player):
+    player_id = player.loc[0].at["keybbref"]
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import numpy as np
+from pybaseball import cache
+from pybaseball import batting_stats_range
+from pybaseball import pitching_stats_range
 
 
-array = np.array
+
+cache.enable()
+
+
+with open('team.txt') as f:
+    team = f.read().splitlines()
+
+# retrieve all players' stats for the 2024 season
+pitcher_data = pitching_stats_range("2023-01-21", "2023-12-31")
+batter_data = batting_stats_range("2023-01-21", "2023-12-31")
+
+batter_data = batter_data[batter_data['Name'].isin(team)]
+pitcher_data = pitcher_data[pitcher_data['Name'].isin(team)]
+
+#batter_data.drop(batter_data[batter_data['Name'] != 'Ronald Acuna Jr.'].index, inplace = True)
+batter_data.to_csv("output.csv", index=False)
+pitcher_data.to_csv("output.csv", index=False, mode="a")
+
+
+print(batter_data)
+print(pitcher_data)
+
+
